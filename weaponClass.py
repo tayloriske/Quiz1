a0513327870db94f78c1100f9806d2aa1e52220c
Johnny Bhojwani <johnnyvb@gmail.com>
2022-02-15 22:59:55 -0600

    Initial Commit

---
+++ weaponClass.py
@@ -0,0 +1,32 @@
+import random
+
+'''
+Create a Weapon Class definition according to the following specs:
+
+Attributes:
+
+name - user supplied
+bullets - random number between 10 and 100000
+speed - user supplied
+range - user supplied
+status - 'Active' (this attribute should be changed to 'Inactive' if bullets 
+                    run out.)
+
+Methods:
+
+Create accessor methods for each attribute.
+
+Create a method named 'fire_bullet' that will simulate
+firing a bullet. This is accomplished by decreasing the number of bullets by 1 
+every time the method is called. When the bullet count reaches zero, it should change
+the attribute 'status' to 'Inactive'
+
+'''
+
+
+
+
+
+
+
+
---
+++ weaponProgram.py
@@ -0,0 +1,67 @@
+import weaponClass as w
+import csv
+
+
+
+
+
+# create a file object to open the file in read mode
+
+
+# create a csv object from the file object
+
+
+#skip the header row
+
+
+#create an empty dictionary named 'weapons_dict'
+
+
+#use a for loop to iterate through every row of the csv file
+
+    #use variables for name,speed and range (optional)
+    
+
+    # create an instance of the weapon object using the 
+    # specs from the csv file (name,speed and range) 
+    
+
+    # append the name and bullet count to 'weapons_dict'
+    
+
+
+    # print out the name of the weapon using the appropriate method of the object 
+    
+    # print out the speed of the weapon using the appropriate method of the object
+    
+    # print out the range of the weapon using the appropriate method of the object
+    
+    # print out the number of bullets of the weapon using the appropriate method of the object
+    
+
+    #use an input statement to halt the program and wait for the user - 
+    input("Press any key to fire the weapon")
+    
+
+    # use an appropriate loop to keep firing the weapon until all bullets run out
+    
+        # call the appropriate method to fire a bullet
+       
+        # print out the bullet count every time the weapon is fired
+        
+
+    
+
+
+#using a loop print out the name and number of bullets from the dictionary
+
+
+
+
+    
+
+
+    
+
+
+
